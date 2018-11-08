89b220c2ad6b5d62614048c85eeecf7c9e79f65b
Justin Reich <justinreich.dev@gmail.com>
2018-11-08 11:40:09 -0600

    add problem 1 solution

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,75 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+
+        // ================ Problem 1
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+//
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+//
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+
+
+        // ================ FizzBuzz
+
+        // Write a program that prints the numbers from 1 to 100.
+        // For multiples of three print “Fizz” instead of the number
+        // For the multiples of five print “Buzz”.
+        // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+
+        // ================ Display a table of powers.
+
+        // Prompt the user to enter an integer.
+        // Display a table of squares and cubes from 1 to the value entered.
+        // Ask if the user wants to continue.
+        // Assume that the user will enter valid data.
+        // Only continue if the user agrees to.
+
+        /*
+
+            What number would you like to go up to? 5
+
+            Here is your table!
+
+            number | squared | cubed
+            ------ | ------- | -----
+            1      | 1       | 1
+            2      | 4       | 8
+            3      | 9       | 27
+            4      | 16      | 64
+            5      | 25      | 125
+
+         */
+
+
+        // ================ Convert given number grades into letter grades.
+
+        // Prompt the user for a numerical grade from 0 to 100.
+        // Display the corresponding letter grade.
+        // Prompt the user to continue.
+        // Assume that the user will enter valid integers for the grades.
+        // The application should only continue if the user agrees to.
+        // Grade Ranges:
+        //
+        // A : 100 - 88
+        // B : 87 - 80
+        // C : 79 - 67
+        // D : 66 - 60
+        // F : 59 - 0
+
+    }
+
+}
