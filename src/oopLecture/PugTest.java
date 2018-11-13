203836fe449a79341ceb04965be274b2b42cdba1
Justin Reich <justinreich.dev@gmail.com>
2018-11-13 11:35:53 -0600

    add lecture notes

--- src/oopLecture/PugTest.java
+++ src/oopLecture/PugTest.java
@@ -2,11 +2,11 @@ package oopLecture;
 
 public class PugTest {
 
-    public static void main(String[] args) {
-        Pug pg = new Pug();
-        pg.changePugAge(10);
-        System.out.println(pg.getPugAge());
-    }
+//    public static void main(String[] args) {
+//        Pug pg = new Pug();
+//        pg.changePugAge(10);
+//        System.out.println(pg.getPugAge());
+//    }
 
 
 }
